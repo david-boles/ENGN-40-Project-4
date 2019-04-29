0f0b9940995f45cf741e974c3abf58f3b92a788e
sageshoyu <seiji.shaw@gmail.com>
2019-04-29 10:58:41 -0400

    started implementation of conversion calcs

--- convert_to_quad_vals.m
+++ convert_to_quad_vals.m
@@ -1,5 +1,20 @@
 % Takes in acceleration ([x, y, z] in m/s^2) and current yaw (radians),
+% assuming frame of reference is in relation to the  room/camera
+% assuming aerospace convention for calculation of yaw compnesation, roll,
+% and pitch
 % returns desired thrust (newtons), pitch, and roll (radians).
-function [thrust, pitch, roll] = convert_to_quad_vals(acceleration, yaw)
 
+function [thrust, pitch, roll] = convert_to_quad_vals(acceleration, yaw)
+    % Rotation matrix to rotate coordinate space to match quadcopter
+    % This allows us to continue calculations as if yaw=0
+    R = [0, -sin(yaw), 0; sin(yaw), cos(yaw), -sin(yaw); 0, 0, 1];
+    a_rotated = R*acceleration;
+    
+    % Compensate for gravity (we still want to be in the air!!)
+    g = 9.81;
+    a_desired = a_rotated + [0, g, 0];
+    
+    % calculate accel norm for thrust
+    thrust = norm(a_desired);
+  
 end
