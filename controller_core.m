b792175fb8f6b061e5d487395e74f59ad88f7c42
David Boles <me@davidbol.es>
2019-04-30 23:37:25 -0400

    New core implementation.

---
+++ controller_core.m
@@ -0,0 +1,6 @@
+% Based on current psi, error ([x, y, z, psi] m or radians), error
+% derivative, and error integral, calculates thrust in quadcopter units,
+% roll and pitch in radians, and yaw acceleration in rad/s^2
+function [thrust, roll, pitch, yaw_acceleration] = controller_core(psi, error, error_derivative, error_integral)
+    
+end
