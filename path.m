6d710d8a4607f3fea6e8dae14542882bae935dc9
IGoByJoe <47760119+IGoByJoe@users.noreply.github.com>
2019-05-01 14:36:21 -0400

    Updated initial flight path

--- path.m
+++ path.m
@@ -27,9 +27,24 @@ function [x, y, z, psi, time_remaining] = path(time, user_parameters)
 end
 
 % Pattern function, starting and ending (p_time = 0 or 1) at [0, 0, 0, 0]
-function [x, y, z, psi] = pattern(p_time)
- x = 0;
- y = 0;
- z = 0;
- psi = 0;
+function [x, y, z, psi] = mypattern(p_time)
+times = [0,0.2,0.4,0.8,1];
+
+waypoint_relative_positions = [0.,0.,0.,0.;...
+                               0.,0,0.9,0.;...
+                              0.0,-0.3,0.9,0.;...
+                              0.0,-0.3,0.9,0.;...
+                              0.0,-0.3,0.3,0.];
+                          
+if p_time < 1
+    relative_position = interp1(times,waypoint_relative_positions,p_time);
+else
+    relative_position = waypoint_relative_positions(end,1:3);
+end
+
+x = relative_position(1);
+y = relative_position(2);
+z = relative_position(3);
+psi = 0;
+
 end
