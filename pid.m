89f96d14fc9d881165624f5b3919bfa0e6917811
IGoByJoe <47760119+IGoByJoe@users.noreply.github.com>
2019-04-30 23:23:31 -0400

    Update pid.m

--- pid.m
+++ pid.m
@@ -1,8 +1,8 @@
 % Takes current error, derivative, and integral as [x, y, z, psi] with
 % units of m, rad (/s or *s respectively), returns desired linear
-% acceleration and yaw velocity
-% ([a_x, a_y, a_z, omega_psi] in m/s^2 and rad/s)
-function [a_x, a_y, a_z, omega_psi] = pid(error, error_derivative, error_integral)
+% acceleration and yaw acceleration
+% ([a_x, a_y, a_z, alpha_psi] in m/s^2 and rad/s^2)
+function [a_x, a_y, a_z, alpha_psi] = pid(error, error_derivative, error_integral)
 
 error_x = error(1);
 error_y = error(2);
@@ -12,15 +12,17 @@ error_psi = error(4);
 error_dx = error_derivative(1);
 error_dy = error_derivative(2);
 error_dz = error_derivative(3);
+error_dpsi = error_derivative(4);
 
 error_ix = error_integral(1);
 error_iy = error_integral(2);
 error_iz = error_integral(3);
+error_ipsi = error_integral(4);
 
 a_x = Kpx*(error_x) + Kdx*error_dx + Kix*error_ix;
 a_y = Kpy*(error_y) + Kdy*error_dy + Kiy*error_iy;
 a_z = Kpz*(error_z) + Kdz*error_dz + Kiz*error_iz;
 
-omega_psi = Kpyaw*(error_psi);
+alpha_psi = Kpyaw*(error_psi) + Kdyaw*(error_dpsi) + Kiyaw*(error_ipsi);
 
 end
