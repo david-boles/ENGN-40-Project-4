8769a7b0ac95fdaeec57df47fca3c9ffc6093289
IGoByJoe <47760119+IGoByJoe@users.noreply.github.com>
2019-04-30 22:26:50 -0400

    Update pid.m

--- pid.m
+++ pid.m
@@ -2,6 +2,7 @@
 % target yaw (in radians) and returns desired linear acceleration and yaw velocity
 % ([a_x, a_y, a_z, omega_psi] in m/s^2 and rad/s)
 function [a_x, a_y, a_z, omega_psi] = pid(position, target_position, yaw, target_yaw, dtime)
+
 x = position(1);
 y = position(2);
 z = position(3);
@@ -10,17 +11,14 @@ xstar = target_position(1);
 ystar = target_position(2);
 zstar = target_position(3);
 
+ex = ex +(xstar-x)*dtime;
+ey = ey +(ystar-y)*dtime;
+ez = ez +(zstar-z)*dtime;
 
+a_x = Kpx*(xstar-x) + (Kdx*(xstar-x))/dtime + Kix*ex;
+a_y = Kpy*(ystar-y) + (Kdy*(ystar-y))/dtime + Kiy*ey;
+a_z = Kpz*(zstar-z) + (Kdz*(zstar-z))/dtime + Kiz*ez;
 
-r_x = xstar-x;
-r_y = ystar-y;
-r_z = zstar-z;
-
-a_x = 2*r_x./dtime^2;
-a_y = 2*r_y./dtime^2;
-a_z = 2*r_z./dtime^2;
-
-angle = target_yaw - yaw;
-omega_psi = angle/dtime;
+omega_psi = Kpyaw*(target_yaw - yaw);
 
 end
