259d68767b1401c75a00d037d22520ae0952e3cb
Charles Shi <shicharles.107@gmail.com>
2019-05-03 22:21:44 -0400

    SImulation without alpha-psi

--- simulation.m
+++ simulation.m
@@ -4,30 +4,30 @@ close all;
 m=constants.MASS_KG;
 %Beta=7.923*10^-6; %Newtons
 g=constants.G; %Newtons
-initial_w=[0,0,0,0,0,0,0,0,0,0,0,0]; %[x,y,z,yaw,vx,vy,vz,omega_psi,ex,ey,ez,e_psi]
+initial_w=[0,0,0,0,0,0,0,0,0,0]; %[x,y,z,yaw,vx,vy,vz,ex,ey,ez]
 
 time_range=[0,30];
 
 [t_values,sol_values] = ode45(@(t,w) diff_eq(t,w,m,g),time_range,initial_w);
 figure
-plot(t_values,sol_values(:,[1 5 9]),"LineWidth",2);
+plot(t_values,sol_values(:,[1 5 8]),"LineWidth",2);
 title("x values");
 legend("x","vx","ex");
 
 figure
-plot(t_values,sol_values(:,[2 6 10]),"LineWidth",2);
+plot(t_values,sol_values(:,[2 6 9]),"LineWidth",2);
 title("y values");
 legend("y","vy","ey");
 
 figure
-plot(t_values,sol_values(:,[3 7 11]),"LineWidth",2);
+plot(t_values,sol_values(:,[3 7 10]),"LineWidth",2);
 title("z values");
 legend("z","vz","ez");
 
 figure
-plot(t_values,sol_values(:,[4 8 12]),"LineWidth",2);
+plot(t_values,sol_values(:,4),"LineWidth",2);
 title("yaw values");
-legend("yaw","omega_psi","e_psi");
+legend("yaw");
 
 end
 
@@ -43,11 +43,9 @@ yaw=w(4);
 vx=w(5);
 vy=w(6);
 vz=w(7);
-omega_psi=w(8);
-ex=w(9);
-ey=w(10);
-ez=w(11);
-e_psi=w(12);
+ex=w(8);
+ey=w(9);
+ez=w(10);
 
 xerror = xstar - x;
 yerror = ystar - y;
@@ -58,15 +56,14 @@ yawerror = yawstar - yaw;
 dxstardt=(xstar-xstarold)/constants.SIM_DERIV_DT;
 dystardt=(ystar-ystarold)/constants.SIM_DERIV_DT;
 dzstardt=(zstar-zstarold)/constants.SIM_DERIV_DT;
-dyawstardt=(yawstar-yawstarold)/constants.SIM_DERIV_DT;
 
 dxerrordt=dxstardt-vx;
 dyerrordt=dystardt-vy;
 dzerrordt=dzstardt-vz;
-dyawerrordt=dyawstardt-omega_psi;
 
-[tau,theta,phi,alpha_psi]=controller_core(yaw, [xerror,yerror,zerror,yawerror],...
-    [dxerrordt,dyerrordt,dzerrordt,dyawerrordt],[ex,ey,ez,e_psi]);
+
+[tau,theta,phi,omega_psi]=controller_core(yaw, [xerror,yerror,zerror],...
+    [dxerrordt,dyerrordt,dzerrordt],[ex,ey,ez], yawerror);
 
 if t >= 0.2
     disp('BAHAHAS');
@@ -80,7 +77,6 @@ dyawdt=omega_psi;
 dvxdt=(sin(theta)*sin(yaw)+cos(theta)*sin(phi)*cos(yaw))*tau/m;
 dvydt=(cos(theta)*sin(phi)*sin(yaw)-sin(theta)*cos(yaw))*tau/m;
 dvzdt=(cos(theta)*cos(phi)*tau/m)-g;
-domega_psidt=alpha_psi;
 
-dwdt=[dxdt;dydt;dzdt;dyawdt;dvxdt;dvydt;dvzdt;domega_psidt;xerror;yerror;zerror;yawerror];
+dwdt=[dxdt;dydt;dzdt;dyawdt;dvxdt;dvydt;dvzdt;xerror;yerror;zerror];
 end
